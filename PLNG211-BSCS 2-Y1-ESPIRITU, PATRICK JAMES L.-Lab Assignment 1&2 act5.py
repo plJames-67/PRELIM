216787a2acb2f264990fafc4bb69708692bdc9d8
plJames-67 <plespiritu4678val@student.fatima.edu.ph>
2026-09-20 18:17:01 +0800

    Create PLNG211-BSCS 2-Y1-ESPIRITU, PATRICK JAMES L.-Lab Assignment 1&2 act5.py

---
+++ PLNG211-BSCS 2-Y1-ESPIRITU, PATRICK JAMES L.-Lab Assignment 1&2 act5.py
@@ -0,0 +1,9 @@
+def last_alphabetically():
+  word1 = input("Enter the first word: ")
+  word2 = input("Enter the second word: ")
+  word3 = input("Enter the third word: ")
+ 
+  last_word = max(word1, word2, word3)
+  print(f"The word that comes last alphabetically is: {last_word}")
+
+last_alphabetically()
