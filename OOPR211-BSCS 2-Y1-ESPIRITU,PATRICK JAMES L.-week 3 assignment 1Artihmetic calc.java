b29c4dde58aa8d8fbb2e5f0de254dd74f764b357
plJames-67 <plespiritu4678val@student.fatima.edu.ph>
2026-09-20 17:55:18 +0800

    Add files via upload

---
+++ OOPR211-BSCS 2-Y1-ESPIRITU,PATRICK JAMES L.-week 3 assignment 1Artihmetic calc.java
@@ -0,0 +1,13 @@
+public class OLFUMV {
+    public static void main(String[] args) {
+        System.out.println("VISION");
+        System.out.println("A premier inclusive university of choice aspiring to improve man as man by developing individuals through a legacy of excellent education and compassionate value formation");
+        System.out.println("MISSION");
+        System.out.println("The Our Lady of Fatima University, together with Fatima Medical Science Foundation, Inc., \nis dedicated to the improvement of man as man through the holistic formation of individuals imbued with knowledge, skills, and virtues.");
+        System.out.println("CORE VALUES");
+        System.out.println("R - Respect");
+        System.out.println("I - Integrity");
+        System.out.println("S - Service");
+        System.out.println("E - Excellence");
+    }
+}
