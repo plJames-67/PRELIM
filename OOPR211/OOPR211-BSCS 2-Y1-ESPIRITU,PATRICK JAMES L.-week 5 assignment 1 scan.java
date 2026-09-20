2ae17c5c80d5f92f5101e7052f8702fa62ecfdba
plJames-67 <plespiritu4678val@student.fatima.edu.ph>
2026-09-20 18:08:09 +0800

    Update OOPR211-BSCS 2-Y1-ESPIRITU,PATRICK JAMES L.-week 5 assignment 1 scan.java

--- OOPR211/OOPR211-BSCS 2-Y1-ESPIRITU,PATRICK JAMES L.-week 5 assignment 1 scan.java
+++ OOPR211/OOPR211-BSCS 2-Y1-ESPIRITU,PATRICK JAMES L.-week 5 assignment 1 scan.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+public class PhraseScanner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first word: ");
+        String first = sc.next();
+
+        System.out.print("Enter second word: ");
+        String second = sc.next();
+
+        System.out.print("Enter third word: ");
+        String third = sc.next();
+
+        System.out.println(first + " " + second + " " + third);
+
+        sc.close();
+    }
+}
